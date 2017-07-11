68865b13513405c2e9b0def49504fdf57ecba898
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-07-11 15:58:40 -0400

    Brand repo to greenelab/scihub-manuscript

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -22,8 +22,8 @@ git remote set-url origin git@github.com:$TRAVIS_REPO_SLUG.git
 
 # Decrypt and add SSH key
 openssl aes-256-cbc \
-  -K $encrypted_f2f00aaf6402_key \
-  -iv $encrypted_f2f00aaf6402_iv \
+  -K $encrypted_8cc93e35fb85_key \
+  -iv $encrypted_8cc93e35fb85_iv \
   -in ci/deploy.key.enc \
   -out ci/deploy.key -d
 eval `ssh-agent -s`
